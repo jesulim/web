2cd4b05320ba1eb2507d2620867143b8f8ebccb6
Ivan Martinez <jesulim@gmail.com>
2024-02-07 00:36:40 -0400

    Fix NyanCat animation and update usage in index page

--- src/components/ui/nyancat.tsx
+++ src/components/ui/nyancat.tsx
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react';
 
 const MovingGif = () => {
   const [position, setPosition] = useState(0);
-  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setPosition(prevPosition => prevPosition + 1);
+      setPosition(prevPosition => {
+        const newPosition = prevPosition + 1;
+        if (newPosition >= window.innerWidth) {
+          clearInterval(interval);
+          return 0;
+        }
+        return newPosition;
+      });
     }, 10);
 
     return () => {
@@ -14,28 +20,8 @@ const MovingGif = () => {
     };
   }, []);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const windowHeight = window.innerHeight;
-      const documentHeight = document.documentElement.scrollHeight;
-      const scrollPosition = window.scrollY;
-
-      if (scrollPosition + windowHeight >= documentHeight) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
-    <div style={{ position: 'relative', left: `${position}px`, display: isVisible ? 'block' : 'none' }}>
+    <div style={{ position: 'relative', left: position }}>
       <img src="./nyancat.gif" alt="Moving Gif" />
     </div>
   );
